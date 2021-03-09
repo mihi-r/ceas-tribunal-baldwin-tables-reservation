298353c9addad77622745f5f5ca9f627472813a5
Jacob Huber <huber2jb@mail.uc.edu>
2021-03-09 11:42:59 -0500

    Fixed SQL query for getting reservations

--- api/get_reserved.php
+++ api/get_reserved.php
@@ -15,14 +15,67 @@ $dateList = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["dateListT
 $monthList = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["monthListText"])));
 $yearList = mysqli_real_escape_string($mysqli, strip_tags(trim($_POST["yearListText"])));
 
+$dateListArray = explode(',', $dateList);
+$monthListArray = explode(',', $monthList);
+$yearListArray = explode(',', $yearList);
+
+
+for($i = 0; $i < count($dateListArray)-1; $i++)
+{
+    if($dateListArray[$i] > $dateListArray[$i+1])
+    {
+        $dateListHigher = array_slice($dateListArray, 0, $i+1);
+        $dateListHigher = implode(',', $dateListHigher);
+        $dateListLower = array_slice($dateListArray, $i+1);
+        $dateListLower = implode(',', $dateListLower);
+        break;
+    }
+    elseif($i == count($dateListArray)-2)
+    {
+        $dateListHigher = $dateListArray;
+        $dateListHigher = implode(',', $dateListHigher);
+        $dateListLower = $dateListArray;
+        $dateListLower = implode(',', $dateListLower);
+    }
+}
+
+if (count($yearListArray) == 1)
+{
+    $yearListLower = implode(',', $yearListArray);
+    $yearListHigher = implode(',', $yearListArray);
+}
+else
+{
+    $yearListLower = implode(',', array_slice($yearListArray, 0, 1));
+    $yearListHigher = implode(',', array_slice($yearListArray, 1));
+}
+
+if (count($monthListArray) == 1)
+{
+    $monthListHigher = implode(',', $monthListArray);
+    $monthListLower = implode(',', $monthListArray);
+}
+else
+{
+    $monthListHigher = implode(',', array_slice($monthListArray, 0, 1));
+    $monthListLower = implode(',', array_slice($monthListArray, 1));
+}
+
+
 if (!isset($resultData)) 
     $resultData = new stdClass();
 $resultData->status = "fail";
 $resultData->data[] = array();
 
-$sql = "SELECT start_time, end_time, date, table_chosen "
-    . "FROM baldwin_tables_reservations "
-    . "WHERE year IN (" . $yearList . ") AND month IN (" . $monthList . ") AND date in (" . $dateList . ")";
+$sql = "SELECT "
+    . "`start_time`, "
+    . "`end_time`, "
+    . "`date`, "
+    . "`table_chosen` "
+    . "FROM `baldwin_tables_reservations` "
+    . "WHERE (`year` = " . $yearListLower . " AND `month` = " . $monthListHigher . " AND `date` in (" . $dateListHigher . "))"
+    . "OR (`year` = " . $yearListHigher . " AND `month` = " . $monthListLower . " AND `date` in (" . $dateListLower . "))";
+
 $result = $mysqli->query($sql);
 
 if ($result) {
