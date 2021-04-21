c8777255fcef45185a77fd998997367ccc2ec8eb
Kai <60765574+KaaiiH@users.noreply.github.com>
2021-04-20 23:31:28 -0400

    Get first and last name from database. (#25)
    * Get name and email for esoc
    
    * fixed the camel case and spaces

---
+++ api/get_info.php
@@ -0,0 +1,28 @@
+<?php
+
+error_reporting(-1);
+
+set_include_path("./includes/");
+require_once("mysqli.php");
+
+$name = "";
+$email = "";
+
+
+$sql = "SELECT admin_name, admin_email FROM baldwin_tables_info ORDER BY id LIMIT 1";
+
+$result = $mysqli->query($sql);
+
+if (!isset($result_data))
+    $result_data = new stdClass();
+
+$result_data->status = "fail";
+
+if ($result) {
+    $result_data->data = $result->fetch_assoc();
+    $result_data->status = "success";
+}
+
+echo json_encode($result_data);
+mysqli_close($mysqli);
+?>
---
+++ scripts/models/getinfo.ts
@@ -0,0 +1,25 @@
+import { ApiStatus, GetInfoApiResponse } from '../types/types';
+export class GetInfo {
+    public name: string;
+    public email: string;
+    
+    public async initData(){
+        const response = await fetch('../api/get_info.php',{
+            method: 'GET'
+        });
+        
+        const data: GetInfoApiResponse = await response.json();
+
+        if (data.status == ApiStatus.Success) {
+            const info = data.data;
+            if (info !== undefined){
+                this.name = info.admin_name;
+                this.email = info.admin_email;
+            }
+        }
+        else {
+            throw new Error('Could not fetch info')
+        }
+        
+    }
+}
